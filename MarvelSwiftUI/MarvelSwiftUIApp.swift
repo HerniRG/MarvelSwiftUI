e0dca41880afc3407adace7d151bc3fc3c30b236
hernirg <hernanrg85@gmail.com>
2024-12-04 15:23:39 +0100

    Initial Commit

---
+++ MarvelSwiftUI/MarvelSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelSwiftUIApp.swift
+//  MarvelSwiftUI
+//
+//  Created by Hernán Rodríguez on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
