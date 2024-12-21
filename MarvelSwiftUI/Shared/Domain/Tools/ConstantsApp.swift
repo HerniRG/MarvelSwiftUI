11a1455bcf37cb28389d8eb10190c1aa6f322ec2
hernirg <hernanrg85@gmail.com>
2024-12-21 16:48:40 +0100

    sin keys

--- MarvelSwiftUI/Shared/Domain/Tools/ConstantsApp.swift
+++ MarvelSwiftUI/Shared/Domain/Tools/ConstantsApp.swift
@@ -6,11 +6,11 @@ public struct ConstantsApp {
     public static let CONS_API_URL = "https://gateway.marvel.com/v1/public"
     
     /// Fixed public API key.
-    public static let CONS_API_PUBLIC_KEY = "ee8f003424754e10d4e3c2d1b782e3b7"
+    public static let CONS_API_PUBLIC_KEY = "YOUR_PUBLIC_KEY_HERE"
     
     /// Fixed timestamp for API requests.
     public static let CONS_API_TS = "1"
     
     /// Fixed hash for API authentication.
-    public static let CONS_API_HASH = "1f80acd6d378d2b8ba1356c2efc0c93f"
+    public static let CONS_API_HASH = "YOUR_HASH_HERE"
 }
